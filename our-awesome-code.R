79f6cc5cb2ff2d5528c1f087ab998b034b659e0b
James Tocknell <aragilar@gmail.com>
2017-12-13 10:24:17 +1100

    Add template

---
+++ our-awesome-code.R
@@ -0,0 +1,19 @@
+awesome_function_1 <- function(argument1) {
+    _______
+}
+
+awesome_function_2 <- function(argument1) {
+    _______
+}
+
+awesome_function_3 <- function(argument1) {
+    _______
+}
+
+awesome_function_4 <- function(argument1) {
+    _______
+}
+
+awesome_function_5 <- function(argument1) {
+    _______
+}
