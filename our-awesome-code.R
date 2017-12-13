5ba52be241a5ff43f2f5a3876e2811e6bc9bbe87
Edwin-Franks <34495636+Edwin-Franks@users.noreply.github.com>
2017-12-13 15:21:51 +1100

    Update our-awesome-code.R

--- our-awesome-code.R
+++ our-awesome-code.R
@@ -14,8 +14,11 @@ awesome_function_4 <- function(argument1) {
     _______
 }
 
-awesome_function_5 <- function(argument1) {
-    _______
+the_awesome_function <- function(argument1,argument2) {
+x1<-argument1
+    x2<-argument2
+    string<- c(x1," is way more awesome than ",x2)
+    return(string)_______
 }
 
-#blabla
+the_awesome_function("Any cat", "that dog!")
